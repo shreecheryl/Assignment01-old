c14926c058044a22df8d63ba05e52b43e8dadc27
shreecheryl <shreecheryl@att.net>
2017-02-09 20:17:42 -0800

    completed files
    completed files

---
+++ script.js
@@ -0,0 +1,196 @@
+// STEP 1
+/*
+var someMonth
+function theMonth
+currentMonth
+var summerMonth
+myLibraryAFunction
+*/
+
+// STEP 2
+/*
+50              // numeric literal expression
+"Cheryl Davis"  // string literal expression
+true            // Boolean lteral expression
+null            // null literal expression
+*/
+
+// STEP 3
+/*
+var anExpression = 3 + 4 * 10;
+var anotherExpression = anExpression + Math.PI;
+*/
+
+// STEP 4
+/*
+var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var strZipCode;
+var intYourAge;
+var strReferralSource;
+var boolMayWeContactYou;
+*/
+
+// STEP 5
+/*
+var strFirstName;
+strFirstName = "Cheryl";
+
+var strLastName = "Davis";
+
+var strAddress = "4750 Noyes St", strCity = "San Diego"; strState = "CA", strZipCode = "92109";
+intYourAge = 54; strReferralSource = "internet"; boolMayWeContactYou = true;
+*/
+
+// STEP 6
+/*
+var firstTest = 10 + "Cheryl";
+window.console.log(firstTest);
+
+var secondTest = true + "Cheryl";
+window.console.log(secondTest);
+
+var thirdTest = 10 + true;
+window.console.log(thirdTest);
+*/
+
+// STEP 7
+/*
+var make = "Chevy";
+window.console.log("The car is a " + make + " " + model + ".");
+var model = "Camaro";
+*/
+
+// STEP 8
+
+// No, it is not valid. To fix it, I would add an escape sequence to I'm
+/*
+var someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someString);
+*/
+
+// STEP 9
+/*
+var x = null;
+var y;
+window.console.log(x);
+window.console.log(y);
+*/
+
+// STEP 10
+/*
+window.console.log(typeof "Cheryl");
+window.console.log(typeof 10);
+window.console.log(typeof true);
+window.console.log(typeof null);
+window.console.log(typeof undefined);
+*/
+
+// STEP 11
+/*
+window.alert("Hello " + "Cheryl Davis" + ", welcome to the JavaScript class!");
+*/
+
+// STEP 12
+/*
+var name = "Cheryl Davis";
+window.alert("Hello " + name + ", welcome to the JavaScript class!");
+
+
+// STEP 13
+
+var course = "JavaScript";
+window.alert("Hello " + name + ", welcome to the " + course + " class!");
+
+
+// STEP 14
+
+window.alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+
+// STEP 15
+
+var promptName = window.prompt("Enter your name");
+window.alert("Hello " + promptName + ".\nWelcome to the " + course + " class!");
+
+
+// STEP 16
+
+var promptCourse = window.prompt("What class are you taking?");
+window.alert("Hello " + promptName + ".\nWelcome to the " + promptCourse + " class!");
+*/
+
+// STEP 17
+/*
+var x = 10;
+var y = 20;
+window.console.log(x + y);
+*/
+
+// STEP 18
+/*
+var x = 20;
+x += 20;
+window.console.log(x);
+*/
+
+// STEP 19
+/*
+var x = 20;
+x *= 5;
+window.console.log(x);
+*/
+
+// STEP 20
+/*
+var x = 20 % 3;
+x /= 1;
+window.console.log(x);
+*/
+
+// STEP 21
+/*
+var x = 10;
+var y = 20;
+window.console.log(x >= 10 && y <= 30);
+*/
+
+// STEP 22
+/*
+var x = 10;
+var y = 20;
+window.console.log(x + y !== 30 || x === 20);
+*/
+
+// STEP 23
+/*
+var widget = new Object();
+window.console.log(typeof widget);
+
+
+// STEP 24
+
+window.console.log(widget instanceof Object);
+
+// STEP 25
+
+window.console.log(widget instanceof String);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
